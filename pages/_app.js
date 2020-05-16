6a9277b025daafbf14be486df5e624d92d523688
Fedor Parfenov <fvpDev@outlook.com>
2020-05-16 19:31:43 -0400

    Add next-secure-headers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/index.css'
+import withSecureHeaders from "next-secure-headers";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withSecureHeaders({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: "'self'",
+      styleSrc: ["'self'", "https://cdn.sanity.io"],
+    },
+  },
+  forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+  // frameGuard: "deny", // default
+  // nosniff: "nosniff", // default
+  // xssProtection: "sanitize", // default
+  referrerPolicy: "same-origin",
+})(MyApp);
