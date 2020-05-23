1b66900d7fdcd0db6f52a2607482d965b738e306
Fedor Parfenov <fvpDev@outlook.com>
2020-05-23 18:27:04 -0400

    Fix viewport (make it zoomable)

--- components/meta/seo.js
+++ components/meta/seo.js
@@ -6,13 +6,12 @@ export default function SEOMeta() {
     <Head>
       <meta charSet='utf-8' />
       <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-      <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+      <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1' />
       <meta
         name="description"
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       />
       <meta name='keywords' content='Keywords' />
-      <meta name="" content={HOME_OG_IMAGE_URL} />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
 
       // <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
