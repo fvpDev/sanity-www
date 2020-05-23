0ba10d5f1850438ab5062d53f678dac463d189a4
Fedor Parfenov <fvpDev@outlook.com>
2020-05-23 19:15:44 -0400

    Add withPWA back

--- next.config.js
+++ next.config.js
@@ -1,33 +1,8 @@
 require('dotenv').config()
-// const withOptimizedImages = require('next-optimized-images')
-// const withPWA = require('next-pwa')
+const withOptimizedImages = require('next-optimized-images')
+const withPWA = require('next-pwa')
 
-module.exports = {
-  optimizeImages: false,
-  optimizeImagesInDev: false,
-  env: {
-    NEXT_EXAMPLE_CMS_SANITY_PROJECT_ID:
-      process.env.NEXT_EXAMPLE_CMS_SANITY_PROJECT_ID,
-  },
-  pwa: {
-    disable: false,
-    register: true,
-    scope: '/',
-    dest: 'public'
-  },
-  webpack: (config, { isServer }) => { // Generate Sitemap + Robots + RSS on build time
-    if (isServer) {
-      require('./utils/generate-sitemap')
-      require('./utils/generate-robots')
-      // require('./utils/generate-rss')
-      console.log(config)
-    }
-    return config
-  }
-}
-
-/*
-withPWA({
+module.exports = withPWA({
   optimizeImages: false,
   optimizeImagesInDev: false,
   env: {
@@ -50,4 +25,3 @@ withPWA({
     return config
   }
 })
-*/
