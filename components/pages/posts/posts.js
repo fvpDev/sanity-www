75100e40d64ced79bd5274e40eed037680f47cd3
Fedor Parfenov <fvpDev@outlook.com>
2020-06-01 16:10:58 -0400

    Add layout option to Posts (grid)

--- components/pages/posts/posts.js
+++ components/pages/posts/posts.js
@@ -1,24 +1,26 @@
 import PostPreview from './_posts/post-preview'
 
-export default function Posts({ posts, all = true }) {
+export default function Posts({ posts, all = true, layout = 'grid' }) {
   return (
     <section>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         {all ? 'All Posts' : 'More Posts'}
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
-        {posts.map(post => (
-          <PostPreview
-            key={post.slug}
-            title={post.title}
-            previewImage={post.coverImage}
-            date={post.date}
-            author={post.author}
-            slug={post.slug}
-            excerpt={post.excerpt}
-          />
-        ))}
-      </div>
+      {layout == 'grid' ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
+          {posts.map(post => (
+            <PostPreview
+              key={post.slug}
+              title={post.title}
+              previewImage={post.coverImage}
+              date={post.date}
+              author={post.author}
+              slug={post.slug}
+              excerpt={post.excerpt}
+            />
+          ))}
+        </div>
+      ) : <div />}
     </section>
   )
 }
